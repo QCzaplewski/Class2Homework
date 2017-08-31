23cc2c2ec9dd34f235db04233717e368a9d36ca7
Quinten <Quinten@Quinten-LAPTOP>
2017-08-31 01:05:52 -0500

    added person constructor

--- src/class2homework/Person.java
+++ src/class2homework/Person.java
@@ -13,4 +13,12 @@ public class Person {
     public String firstName;
     public String lastName;
     public int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    
+    
 }
